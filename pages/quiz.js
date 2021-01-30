f29044a5d93f1bf039cf2b36ea99ea1a5422e3c0
Josafá Veríssimo <josafaverissimo98@gmail.com>
2021-01-29 23:36:40 -0300

    FIX: Tela de loading readicionada

--- pages/quiz.js
+++ pages/quiz.js
@@ -163,7 +163,7 @@ const screenStates = {
 };
 
 export default function QuizPage() {
-  const [screenState, setScreenState] = React.useState(screenStates.QUIZ);
+  const [screenState, setScreenState] = React.useState(screenStates.LOADING);
   const [results, setResults] = React.useState([]);
   const [currentQuestion, setCurrentQuestion] = React.useState(0);
   const question = db.questions[currentQuestion];
